7adb4f6e5c666f4f85687bcda9052f0ba8510185
miau <mauricio@pymeweb.mx>
2025-05-09 20:00:02 -0600

    ajustes sql

--- pymengine.sql
+++ pymengine.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 5.2.1deb3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 01-09-2021 a las 18:22:57
--- Versión del servidor: 5.7.24
--- Versión de PHP: 7.3.7
+-- Tiempo de generación: 10-05-2025 a las 01:58:57
+-- Versión del servidor: 8.0.41-0ubuntu0.24.04.1
+-- Versión de PHP: 8.2.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,8 +18,54 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `site_g73002`
+-- Base de datos: `miau_tools`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `label`
+--
+
+CREATE TABLE `label` (
+  `label_id` int NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paper`
+--
+
+CREATE TABLE `paper` (
+  `paper_id` int NOT NULL,
+  `title` varchar(1024) NOT NULL,
+  `authors` text NOT NULL,
+  `labels` text NOT NULL,
+  `abstract` text NOT NULL,
+  `metodology` text NOT NULL,
+  `conclusions` text NOT NULL,
+  `bibliography` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `document` mediumtext NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `path` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reference`
+--
+
+CREATE TABLE `reference` (
+  `reference_id` int NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `autors` varchar(255) DEFAULT NULL,
+  `type` enum('libro','articulo_revista','articulo_periodico','fuente_web','tesis','informe','capitulo_libro','ley','norma','otros') NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -29,12 +74,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `site_data` (
-  `site_data_id` int(11) NOT NULL,
+  `site_data_id` int NOT NULL,
   `name` varchar(255) NOT NULL,
   `value` longtext NOT NULL,
-  `status` tinyint(4) NOT NULL,
+  `status` tinyint NOT NULL,
   `fecha` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -43,70 +88,47 @@ CREATE TABLE `site_data` (
 --
 
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int NOT NULL,
   `name` varchar(32) DEFAULT NULL,
   `lastname` varchar(32) DEFAULT NULL,
   `user` varchar(32) DEFAULT NULL,
   `email` varchar(256) DEFAULT NULL,
   `pwd` varchar(64) DEFAULT NULL,
   `salt` binary(64) DEFAULT NULL,
-  `level` int(11) DEFAULT NULL,
+  `level` int DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `last_login` datetime DEFAULT NULL,
   `active` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
 INSERT INTO `user` (`user_id`, `name`, `lastname`, `user`, `email`, `pwd`, `salt`, `level`, `created`, `last_login`, `active`) VALUES
-(1, 'Angel', 'Trejo', 'root', 'ing.link.316@gmail.com', '039b1a63d4b74137adb6555397fb02462b114f4c46b8f47dba8e48eff67136ca', 0xf130a9c6ac02676b9d75f8ae5e2d68ba5f0e265d9edc7e5d77b1cb6b77b8aee591ad7be221a1bb4b39950e585cb548475e2f855307078e73eb4d2962f33258e0, 1, NULL, '2021-09-01 12:52:56', 1),
-(2, 'Test', 'qwe', 'develop', 'desarrollo@losdeidea.com.mx', '6eb32b2ea22aaa3bcdea24a6023b7eb6395f2a6f4bdb3ef0dbe028d6ec69e0ff', 0x364ad8fd4749a8708711d6195ce404afd8d9e026241d99b87d0d5a23b4a35927aaf6b886dc80c434b0401426f91282f6f14f9521b02e97ac170f8f6429259aa8, 2, '2021-07-20 01:02:39', '2021-07-23 17:55:25', 1);
-
+(1, NULL, NULL, 'admin', NULL, 'f78707fa8c6113fee11fe09b39941df5fd6ae022e6214aaf70cbeb25647afccc', 0xc0363b272299065ba8d278910e87441f7f0ba5681193d36046c53ecc4fab88d1a0d868248e14e2f24654a4e7160e40160143c7b6010d1a037562169d83f6d314, 1, NULL, '2025-05-09 13:13:52', 1);
 
 --
--- Estructura de tabla para la tabla `paper`
+-- Índices para tablas volcadas
 --
-DROP TABLE IF EXISTS `paper`;
-CREATE TABLE `paper` (
-  `paper_id` int(11) NOT NULL,
-  `title` varchar(1024) NOT NULL,
-  `authors` text NOT NULL,
-  `labels` text NOT NULL,
-  `abstract` text NOT NULL,
-  `metodology` text NOT NULL,
-  `conclusions` text NOT NULL,
-  `bibliography` text NOT NULL,
-  `created` datetime DEFAULT CURRENT_TIMESTAMP;,
-  `path` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Estructura de tabla para la tabla `paper`
+-- Indices de la tabla `label`
 --
-DROP TABLE IF EXISTS `label`;
-CREATE TABLE `label` (
-  `label_id` int(11) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE `label`
+  ADD PRIMARY KEY (`label_id`);
+
 --
--- Estructura de tabla para la tabla `paper`
+-- Indices de la tabla `paper`
 --
-DROP TABLE IF EXISTS `bibliography`;
-CREATE TABLE `bibliography` (
-  `bibliography_id` int(11) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  `autors` VARCHAR(255),
-  `type` ENUM('libro', 'articulo_revista', 'articulo_periodico', 'fuente_web', 'tesis', 'informe', 'capitulo_libro', 'ley', 'norma', 'otros') NOT NULL,
-  `created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE `paper`
+  ADD PRIMARY KEY (`paper_id`);
 
-
-
-
-
+--
+-- Indices de la tabla `reference`
+--
+ALTER TABLE `reference`
+  ADD PRIMARY KEY (`reference_id`);
 
 --
 -- Indices de la tabla `site_data`
@@ -121,55 +143,40 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Indices de la tabla `paper`
---
-ALTER TABLE `paper`
-  ADD PRIMARY KEY (`paper_id`);
---
--- Indices de la tabla `label`
---
-ALTER TABLE `label`
-  ADD PRIMARY KEY (`label_id`);
---
--- Indices de la tabla `bibliography`
---
-ALTER TABLE `bibliography`
-  ADD PRIMARY KEY (`bibliography_id`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `label`
+--
+ALTER TABLE `label`
+  MODIFY `label_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `paper`
+--
+ALTER TABLE `paper`
+  MODIFY `paper_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reference`
+--
+ALTER TABLE `reference`
+  MODIFY `reference_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `site_data`
 --
 ALTER TABLE `site_data`
-  MODIFY `site_data_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `site_data_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
---
--- AUTO_INCREMENT de la tabla `paper`
---
-ALTER TABLE `paper`
-  MODIFY `paper_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `label`
---
-ALTER TABLE `label`
-  MODIFY `label_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `bibliography`
---
-ALTER TABLE `bibliography`
-  MODIFY `bibliography_id` int(11) NOT NULL AUTO_INCREMENT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
